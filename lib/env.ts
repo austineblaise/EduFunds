ac06d1a7ddf6a5dc4175309e241ae2511729401f
austineblaise <austineblaise@gmail.com>
2025-08-06 02:47:57 +0100

    fixes

--- lib/env.ts
+++ lib/env.ts
@@ -1,4 +1,11 @@
-// env.ts
-export const env = {
-  NEXT_PUBLIC_URL: process.env.NEXT_PUBLIC_URL || "http://localhost:3000",
-};
+import { z } from "zod";
+
+const envSchema = z.object({
+  NEXT_PUBLIC_URL: z.string().url(),
+  NEXT_PUBLIC_APP_ENV: z.enum(["development", "production", "staging"]),
+});
+
+export const env = envSchema.parse({
+  NEXT_PUBLIC_URL: process.env.NEXT_PUBLIC_URL,
+  NEXT_PUBLIC_APP_ENV: process.env.NEXT_PUBLIC_APP_ENV,
+});
